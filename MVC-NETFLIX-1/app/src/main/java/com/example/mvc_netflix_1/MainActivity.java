a48bfca764d54c6778db38cf9ec453bb652a9f71
alextejero28 <alextejero3@gmail.com>
2023-01-19 16:13:26 +0100

    subiendo mvc completado V0.2.0

--- MVC-NETFLIX-1/app/src/main/java/com/example/mvc_netflix_1/MainActivity.java
+++ MVC-NETFLIX-1/app/src/main/java/com/example/mvc_netflix_1/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mvc_netflix_1.controller.StudentController;
 import com.example.mvc_netflix_1.model.StudentWS;
 import com.example.mvc_netflix_1.model.pojo.Student;
+import com.example.mvc_netflix_1.view.ViewLogin;
 
 public class MainActivity extends AppCompatActivity {
     private StudentController controller;
     private static MainActivity view;
+    private ViewLogin viewLogin;
     private StudentWS model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // CARGAR VIEW
         view = this;
         // CARGAR MODEL
-
+        model = new StudentWS();
         // MODEL + VIEW
         controller = new StudentController(model, view);
         // CASO DE USO -> OBTENER ESTUDIANTE
@@ -30,25 +35,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printStudentDetails(Student student){
-        Log.d("MainActivity - NAME", student.getName());
+        /*Log.d("MainActivity - NAME", student.getName());
         Log.d("MainActivity - SURNAME", student.getSurname());
+        */
+
     }
 
     private void initComponents() {
-        edtUser = findViewById(R.id.edtUser);
-        edtPassword = findViewById(R.id.edtPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
+        EditText edtUser = findViewById(R.id.edtUser);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+        Button enviarbtn = findViewById(R.id.enviarbtn);
+        enviarbtn.setOnClickListener(new View.OnClickListener() {
+           @Override
             public void onClick(View view) {
                 String email = edtUser.getText().toString();
                 String password = edtPassword.getText().toString();
                 if (!email.equals("") && email.length() > 0 &&
                         !password.equals("") && password.length() > 0) {
-
-
-                    ServiceLogin.mostrarLogin(email, password);
+                    viewLogin.mostrarLogin(email, password);
                 }
+                model.getStudentWS().setName(edtUser.toString());
             }
         });
     }
--- MVC-NETFLIX-1/app/src/main/java/com/example/mvc_netflix_1/model/StudentWS.java
+++ MVC-NETFLIX-1/app/src/main/java/com/example/mvc_netflix_1/model/StudentWS.java
@@ -13,4 +13,6 @@ public class StudentWS {
         return student;
 
     }
+
+
 }
