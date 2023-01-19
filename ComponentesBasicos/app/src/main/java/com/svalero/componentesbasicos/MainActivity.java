91f75f90b28354138376220e42dab84bf7daf499
alextejero28 <alextejero3@gmail.com>
2023-01-20 00:00:10 +0100

    subiendo skill 2 V0.1.0

---
+++ ComponentesBasicos/app/src/main/java/com/svalero/componentesbasicos/MainActivity.java
@@ -0,0 +1,19 @@
+package com.svalero.componentesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),
+                "Hola este es un Toast", Toast.LENGTH_SHORT).show();
+
+    }
+}
